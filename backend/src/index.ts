f56d34529e5e68fccbc73a940b3bf5d84112a0d3
Shashank <shashankintouch@gmail.com>
2024-02-07 20:40:04 +0530

    updated hotel functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,6 +35,9 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(3000, () => {
   console.log("Connected to port 3000");
---
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("test12@testing.com");
+
+  await page.locator("[name=password]").fill("12345678");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful")).toBeVisible();
+});
+
+test("should allow user to add hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}add-hotel`);
+
+  await page.locator('[name="name"]').fill("Test Hotel");
+  await page.locator('[name="city"]').fill("Test City");
+  await page.locator('[name="country"]').fill("Test Country");
+  await page
+    .locator('[name="description"]')
+    .fill(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    );
+  await page.locator('[name="pricePerNight"]').fill("100");
+
+  //await page.selectOption('[select[name="starRating"]', "3");
+  await page.selectOption('select[name="starRating"]', "3");
+
+  await page.getByText("Luxury").click();
+  await page.getByLabel("Free Wifi").check();
+  await page.getByLabel("Parking").check();
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("2");
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "scooter.png"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel saved successfully")).toBeVisible();
+});
