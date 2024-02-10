59d5083de80f7dac62ad3af19e861d767cde224c
Shashank <shashankintouch@gmail.com>
2024-02-10 20:41:58 +0530

    added filter tests

---
+++ e2e-tests/tests/search-hotels.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173/";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("test12@testing.com");
+  await page.locator("[name=password]").fill("12345678");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+});
+
+test("should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you travelling").fill("test_country");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page.getByText("Hotels Found in test_country")).toBeVisible();
+  await expect(page.getByText("test_hotel")).toBeVisible();
+});
